2c774dff105456989e8b74ed2ccf15ecf9dbb39a
xiaowei <xiaowei@lbxtech.com>
2018-06-06 15:00:39 +0800

    label auto adjust size for its text when using AutoLayout and Frame

--- Calculate-Text-Size/Calculate-Text-Size/ViewController.swift
+++ Calculate-Text-Size/Calculate-Text-Size/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let autoLayoutLabel = UILabel()
+    let frameLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        createAutoLayoutLabel()
+        createFrameLabel()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func createAutoLayoutLabel() {
+        autoLayoutLabel.text = "这是一段多行文本，这是一段多行文本，这是一段多行文本，这是一段多行文本，这是一段多行文本"
+        autoLayoutLabel.textColor = .red
+        autoLayoutLabel.numberOfLines = 0
+        autoLayoutLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(autoLayoutLabel)
+        autoLayoutLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8.0).isActive = true
+        autoLayoutLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 44.0).isActive = true
+        autoLayoutLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8.0).isActive = true
     }
-
-
+    
+    private func createFrameLabel() {
+        frameLabel.text = "这是一段多行文本，这是一段多行文本，这是一段多行文本，这是一段多行文本，这是一段多行文本"
+        frameLabel.textColor = .green
+        frameLabel.numberOfLines = 0
+        view.addSubview(frameLabel)
+        let size = frameLabel.sizeThatFits(CGSize(width: view.bounds.size.width - 16.0, height: 1000))
+        frameLabel.frame = CGRect(x: 8, y: 200, width: size.width, height: size.height)
+    }
 }
 
